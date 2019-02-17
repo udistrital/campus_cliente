3a0538fc30ab516a154c0ce2d71ff18a0858af4b
BOTOOM <eddiazr@correo.udistrital.edu.co>
2019-02-16 21:45:04 -0500

    limpieza del modulo

--- src/app/pages/tabla_notas/tabla_notas.module.ts
+++ src/app/pages/tabla_notas/tabla_notas.module.ts
@@ -2,16 +2,12 @@ import { TablaNotasRoutingModule, routedComponents } from './tabla_notas-routing
 import { NgModule } from '@angular/core';
 import { ThemeModule } from '../../@theme/theme.module';
 import { IdiomaService } from '../../@core/data/idioma.service';
-import { Ng2SmartTableModule } from 'ng2-smart-table';
-import { ToasterModule } from 'angular2-toaster';
 import { SharedModule } from '../../shared/shared.module';
 
 @NgModule({
   imports: [
     ThemeModule,
     TablaNotasRoutingModule,
-    Ng2SmartTableModule,
-    ToasterModule,
     SharedModule,
   ],
   declarations: [
