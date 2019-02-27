061d27bc209d22f0e04ee5425a11da9a0bdda981
BOTOOM <eddiazr@correo.udistrital.edu.co>
2019-02-26 23:42:18 -0500

    se realiza la consulta al filtro y se limpia el filtro

--- src/app/pages/tabla_notas/list-tabla_notas/list-tabla_notas.component.ts
+++ src/app/pages/tabla_notas/list-tabla_notas/list-tabla_notas.component.ts
@@ -5,6 +5,7 @@ import Swal from 'sweetalert2';
 import { IAppState } from '../../../@core/store/app.state';
 import { Store } from '@ngrx/store';
 import { ListService } from '../../../@core/store/services/list.service';
+import { HttpErrorResponse } from '@angular/common/http';
 
 @Component({
   selector: 'ngx-list-tabla-notas',
@@ -34,6 +35,48 @@ export class ListTablaNotasComponent implements OnInit {
 
   ngOnInit() {
   }
+
+  Filtrar() {
+    console.info('boton de filtrar');
+    if ( (this.selectedValueIdioma !== undefined && this.selectedValueIdioma !== 0 )
+    && (this.selectedValuePeriodo !== undefined && this.selectedValuePeriodo !== 0 ) ) {
+      this.BusquedaDatos(`notas_idioma/?query=Idioma:${this.selectedValueIdioma['Id']},Periodo:${this.selectedValuePeriodo['Id']}`);
+    }
+  }
+
+  ClearFiltro() {
+    console.info('boton de limpiar filtro');
+    this.selectedValueIdioma = '--Seleccionar--'
+    this.selectedValueIdioma = 0;
+    this.selectedValuePeriodo = '--Seleccionar--'
+    this.selectedValuePeriodo = 0;
+  }
+
+  BusquedaDatos(query) {
+    console.info(query);
+    if (query) {
+      this.idiomaService.get(query).subscribe(res => {
+        if (res !== null) {
+          console.info(res)
+        } else {
+          Swal({
+            type: 'info',
+            title: this.translate.instant('GLOBAL.warning'),
+            text: `no se encontraron resultados`,
+            confirmButtonText: this.translate.instant('GLOBAL.aceptar'),
+          });
+        }
+  },
+  (error: HttpErrorResponse) => {
+    Swal({
+      type: 'error',
+      title: error.status + '',
+      text: this.translate.instant('ERROR.' + error.status),
+      confirmButtonText: this.translate.instant('GLOBAL.aceptar'),
+    });
+   });
+  }
+}
 
   public loadLists() {
     this.store.select((state) => state).subscribe(
